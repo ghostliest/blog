4ed070afb227047906873ff7598b8b32f2782e08
ghostliest <LukEv0nz@gmail.com>
2022-10-12 14:55:55 +0300

    Reaction service

---
+++ client/src/services/reactions/reactions.interface.ts
@@ -0,0 +1,21 @@
+export type ReactionsCategoryType = "readingList" | "like";
+
+interface IReactionItemResponse {
+  count: number;
+  thisUser: boolean | undefined;
+}
+
+export interface IReactionsResponse {
+  readingList: IReactionItemResponse;
+  like: IReactionItemResponse;
+}
+
+export interface IQuery {
+  postId: number;
+  userId: number | undefined;
+}
+
+export interface IAddOrDeleteReactionsResponse {
+  category: ReactionsCategoryType;
+  result: "create" | "destroy";
+}
