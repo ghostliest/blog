74e1cb4f7efe474b3d3d29773490eab23e3ebb6f
ghostliest <LukEv0nz@gmail.com>
2022-10-13 18:43:43 +0300

    Fixed: spinner margins

--- client/src/components/ui/Spinner/Spinner.tsx
+++ client/src/components/ui/Spinner/Spinner.tsx
@@ -5,7 +5,16 @@ import styles from "./Spinner.module.scss";
 
 export const Spinner = ({ fullScreen = false, className = "", style, size = "s" }: SpinnerProps) => {
   return (
-    <div className={cn(styles.spinnerWrapper, className)} style={{ ...style }}>
+    <div
+      className={cn(
+        styles.spinnerWrapper,
+        {
+          ["p-8"]: size === "m",
+        },
+        className
+      )}
+      style={{ ...style }}
+    >
       <div className={cn(styles.spinnerContainer, { [styles.spinnerFullscreen]: fullScreen })}>
         <div className={cn(styles.spinner)}>
           <SpinnerIcon className={size === "s" ? "scale-[1]" : size === "m" ? "scale-[3]" : "scale-[6]"} />
